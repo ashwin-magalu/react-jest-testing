bdd7872a533e90f0a3f531e970fa14a9c01977dc
ashwin-magalu <ashwin.magalu@gmail.com>
2021-12-02 13:57:24 +0530

    Initial commit

---
+++ src/reducers/questions.spec.js
@@ -0,0 +1,33 @@
+/**
+ * Placing every test inside a "describe" block is not a requirement but makes for a good, clean means of organization.
+ */
+import { questions } from "./questions";
+
+/**
+ * Testing Reducers is the easiest part of a a React / Redux application.
+ * Reducers are pure functions, so they never depend on dependencies that need to be mocked, and
+ * can be relied upon to return the same data given similar arguments.
+ */
+describe("The questions reducer", () => {
+  /**
+   * In this case both toBe and toEqual will return true. However, the real test in this example is toBe, since we want literally the same object both times.
+   *
+   */
+  it("Should return the same state on an inapplicable action", () => {
+    const state = ["foo", "bar"];
+    const newState = questions(state, { type: "UNDEFINED_ACTION" });
+    expect(newState).toBe(state);
+    expect(newState).toEqual(state);
+  });
+  /** Here a map is used to allow a simple one-liner as an equality check.
+   * TODO... is there a more applicable matcher available?
+   */
+  it("Should add a new questions to the list on a FETCHED_QUESTION action", () => {
+    const state = [{ question_id: "foo" }, { question_id: "bar" }];
+    const newState = questions(state, {
+      type: `FETCHED_QUESTION`,
+      question: { question_id: "baz" },
+    });
+    expect(newState.map((q) => q.question_id)).toEqual(["baz", "foo", "bar"]);
+  });
+});
